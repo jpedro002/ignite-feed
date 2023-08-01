991514cea38fff28e3571ee4da0f162db7584df8
jpedro002 <joaopsilvavolei@gmail.com>
2023-07-31 21:41:28 -0500

    ajuste para tornar responsivo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
       <Header/>
-      <div className="max-w-6xl my-8 mx-auto py-0 px-4 grid grid-cols-[16rem_1fr] items-start gap-8  ">
+      <div className="max-w-6xl my-8 mx-auto py-0 px-4 grid lg:grid-cols-[16rem_1fr] grid-cols-1  items-start gap-8  ">
         <SideBar/>
         <main className='space-y-8'>
           <Post />
